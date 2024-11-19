b5e03c63d17e31b85a90ab1a35c0645d84dab8ac
Jannik <1337741710@qq.com>
2024-11-20 00:08:24 +0800

    docs: 文档更新

--- .vitepress/config/getGuideSidebar.ts
+++ .vitepress/config/getGuideSidebar.ts
@@ -5,8 +5,9 @@ export const getGuideSidebar = (): DefaultTheme.Sidebar => {
     {
       text: "指南",
       items: [
-        { text: "安装", link: "/guide/installation/" },
         { text: "更新日志", link: "/guide/log/" },
+        { text: "安装", link: "/guide/installation/" },
+        { text: "快速开始", link: "/guide/quickstart/" },
         { text: "暗黑模式", link: "/guide/dark/" },
       ],
     },
--- guide/installation/index.md
+++ guide/installation/index.md
@@ -3,7 +3,10 @@
 ## 选择你喜欢的安装方式
 
 ```shell
+
 npm i versakit-ui
+
 yarn add versakit-ui
+
 pnpm add versakit-ui
 ```
